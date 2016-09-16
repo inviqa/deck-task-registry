982ed12a40acd848d6667577f884cbdc5520bcb0
Matt Chapman <chapabu101@gmail.com>
2016-09-16 01:25:40 +0100

    Remoed jasmine tests and replaces Jasmine with Mocha/Chai

--- theme/spec/support/runner.js
+++
@@ -1,7 +0,0 @@
-const Jasmine = require('jasmine');
-const jasmine = new Jasmine();
-const SpecReporter = require('jasmine-spec-reporter');
-
-jasmine.loadConfigFile('spec/support/jasmine.json');
-jasmine.addReporter(new SpecReporter());
-jasmine.execute();
