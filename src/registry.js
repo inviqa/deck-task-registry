3fbcfb42795d3d42436b7d0780d7cbc936663f75
Matt Chapman <chapabu101@gmail.com>
2016-11-29 09:19:33 +0000

    Fixed registry so gulp shows tasks

--- src/registry.js
+++ src/registry.js
@@ -1,51 +1,69 @@
-  'use strict';
+'use strict';
 
-  const defaultRegistry = require('undertaker-registry');
-  const configParser = require('./config/configParser');
+const DefaultRegistry = require('undertaker-registry');
+const configParser = require('./config/configParser');
 
-  class InviqaDrupalRegistry extends defaultRegistry {
+/**
+ * Custom Gulp registry containing Drupal tasks.
+ *
+ * @class InviqaDrupalRegistry
+ * @extends {DefaultRegistry}
+ */
+class InviqaDrupalRegistry extends DefaultRegistry {
 
-    constructor(conf) {
-      super();
+  /**
+   * Creates an instance of InviqaDrupalRegistry.
+   *
+   * @param {object|ConfigParser} conf Configuration object.
+   *
+   * @memberOf InviqaDrupalRegistry
+   */
+  constructor(conf) {
+    super();
 
-      // If we've got no config, then bomb out.
-      if (typeof conf === 'undefined') {
-        throw new Error('No config provided.');
-      }
-
-      // If we've got config, but it's just an object, then make it a configParser instance.
-      if (conf.constructor.name !== 'configParser') {
-        conf = new configParser(conf);
-      }
-
-      this.conf = conf;
-
+    // If we've got no config, then bomb out.
+    if (typeof conf === 'undefined') {
+      throw new Error('No config provided.');
     }
 
-    init () {
-
-      // SASS/CSS.
-      this.set('build:styles', require('./styles/buildStyles')(this.conf));
-
-      // SCRIPTS.
-      this.set('build:scripts', require('./scripts/buildScripts')(this.conf));
-      this.set('lint:scripts', require('./scripts/lintScripts')(this.conf));
-
-      // OTHER ASSETS.
-      this.set('build:images', require('./assets/buildImages')(this.conf));
-      this.set('build:fonts', require('./assets/buildFonts')(this.conf));
-      this.set('build:clean', require('./build/clean')(this.conf));
-
-      // BUILD.
-      this.set('build', require('./build/build')(this.conf));
-
-      // ANCILLIARY.
-      this.set('generate-subtheme', require('./other/generateTheme')());
-      this.set('watch', require('./other/watch')(this.conf));
-      this.set('default', require('./other/default')(this.conf));
-
+    // If we've got config, but it's just an object, then make it a configParser instance.
+    if (conf.constructor.name !== 'configParser') {
+      conf = new configParser(conf);
     }
+
+    this.conf = conf;
 
   }
 
-  module.exports = InviqaDrupalRegistry;
+  /**
+   * @inheritdoc
+   *
+   * @memberOf InviqaDrupalRegistry
+   */
+  init(undertaker) {
+
+    // SASS/CSS.
+    undertaker.task('build:styles', require('./styles/buildStyles')(this.conf));
+
+    // SCRIPTS.
+    undertaker.task('build:scripts', require('./scripts/buildScripts')(this.conf));
+    undertaker.task('lint:scripts', require('./scripts/lintScripts')(this.conf));
+
+    // OTHER ASSETS.
+    undertaker.task('build:images', require('./assets/buildImages')(this.conf));
+    undertaker.task('build:fonts', require('./assets/buildFonts')(this.conf));
+    undertaker.task('build:clean', require('./build/clean')(this.conf));
+
+    // BUILD.
+    undertaker.task('build', require('./build/build')(this.conf));
+
+    // ANCILLIARY.
+    undertaker.task('generate-subtheme', require('./other/generateTheme')());
+    undertaker.task('watch', require('./other/watch')(this.conf));
+    undertaker.task('default', require('./other/default')(this.conf));
+
+  }
+
+}
+
+module.exports = InviqaDrupalRegistry;
