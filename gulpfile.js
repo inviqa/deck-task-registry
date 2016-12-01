3897d2efd397b2d99c578c3539d640b20f15f95e
Matt Chapman <chapabu101@gmail.com>
2016-12-01 07:34:31 +0000

    Removed example config and gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const gulp = require('gulp');
-const inviqaDrupalRegistry = require('./src/registry');
-const config = require('./config');
-const registry = new inviqaDrupalRegistry(config);
-
-gulp.registry(registry);
