b03cc32aaaabf2b0bdb61c2d9a00794da12b37dc
Matt Chapman <chapabu101@gmail.com>
2017-02-15 18:09:35 +0000

    Added Typescript compilation to JS build task.

--- src/scripts/buildScripts.js
+++ src/scripts/buildScripts.js
@@ -5,6 +5,7 @@ const path = require('path');
 const uglify = require('gulp-uglify');
 const gulpIf = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
 
 function buildScripts(conf) {
 
@@ -12,12 +13,24 @@ function buildScripts(conf) {
     mangle: false
   };
 
-  const jsSrc = path.join(conf.themeConfig.root, conf.themeConfig.js.src, '**', '*.js');
+  const typeScriptConf = {
+    target: 'es5',
+    allowJs: true
+  };
+
+  const jsSrc = path.join(conf.themeConfig.root, conf.themeConfig.js.src, '**', '*.[tj]s');
   const jsDest = path.join(conf.themeConfig.root, conf.themeConfig.js.dest);
+
+  let useTypescript = false;
+
+  if (conf.themeConfig.js.hasOwnProperty('es2015')) {
+    useTypescript = conf.themeConfig.js.es2015;
+  }
 
   // Build the theme scripts.
   return gulp.src(jsSrc)
     .pipe(gulpIf(!conf.productionMode, sourcemaps.init()))
+    .pipe(gulpIf(useTypescript, ts(typeScriptConf)))
     .pipe(gulpIf(conf.productionMode, uglify(uglifyConf)))
     .pipe(gulpIf(!conf.productionMode, sourcemaps.write('.')))
     .pipe(gulp.dest(jsDest));
